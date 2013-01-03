e8caeaa62946051dfde7fe9894318c15cda7a693
Vic Mortelmans <vicmortelmans@gmail.com>
2013-01-03 22:45:50 +0100

    adding prayers and PLS-files for radio

--- js/credomobiel.js
+++ js/credomobiel.js
@@ -2,3 +2,10 @@ $(document).bind("mobileinit", function() {
     $.mobile.page.prototype.options.addBackBtn = true;
     $.mobile.page.prototype.options.backBtnText = "terug";
 });    
+
+$(document).on( 'pageinit',function(event){
+    $("input[type='radio']").on( "change", function(event, ui) {
+      $('div.' + $(this).attr('name')).hide();
+      $('div.' + $(this).attr('name') + '.' + $(this).attr('id')).show();
+    });
+});
