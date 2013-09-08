62619224bca33fdb2d336287162a4da69f4b0ae9
Vic Mortelmans <vicmortelmans@gmail.com>
2013-09-08 20:51:25 +0200

    small fixes

--- js/credomobiel.js
+++ js/credomobiel.js
@@ -1,11 +1,20 @@
-$(document).bind("mobileinit", function() {
-    $.mobile.page.prototype.options.addBackBtn = true;
-    $.mobile.page.prototype.options.backBtnText = "terug";
-});    
-
 $(document).on( 'pageinit',function(event){
     $("input[type='radio']").on( "change", function(event, ui) {
       $('.' + $(this).attr('name')).hide();
       $('.' + $(this).attr('name') + '.' + $(this).attr('id')).show();
     });
+});
+/*
+ following section (at least the part in the 'if') fixes flickering of page transitions on Android
+ and must be imported between jQuery and jQuery Mobile
+ */
+$(document).bind("mobileinit", function()
+{
+  $.mobile.page.prototype.options.addBackBtn = true;
+  $.mobile.page.prototype.options.backBtnText = "terug";
+  if (navigator.userAgent.indexOf("Android") != -1)
+  {
+    $.mobile.defaultPageTransition = 'none';
+    $.mobile.defaultDialogTransition = 'none';
+  }
 });
